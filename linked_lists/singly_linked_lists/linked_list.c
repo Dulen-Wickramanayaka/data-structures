1715444601af6d4f532ff3ae0e9ac7db9f021157
captn_nemo <dulenwickramanayaka22@gmail.com>
2024-01-30 12:50:52 +0530

    add more functions - prepend,append,appending in sorted order

--- linked_lists/singly_linked_lists/linked_list.c
+++ linked_lists/singly_linked_lists/linked_list.c
@@ -8,58 +8,99 @@ typedef struct node
 } 
 node;
 
-node *make_list(int head); // adding the first node of the list
-node *insert_node(int value, node *list); // inserting nodes to the beginning of the list
-void print_list(node *list);
-void free_list(node *list);
+node *prepend_node(node *n, node *head); // prepending nodes
+node *append_node(node *n, node *head); // appending nodes
+node *append_sorted(node *n, node *head);
+void print_list(node *head); // printing the list
+void free_list(node *head); // free the list
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
         printf("USAGE ./linked_list ..Space_seperated_numbers");
+        return(1);
     }
 
     node *list = NULL; // initailizing list
     for (int i = 1; i < argc; i++)
     {
         int number = atoi(argv[i]);
-        if (list == NULL) // if list isn't exisitng
+
+        // creating the current node
+        node *n = malloc(sizeof(node)); // dynamically allocating memory for current node
+        if (n == NULL)
         {
-            list = make_list(number); // make this node the first 
+            printf("memory allocation failed!");
+            return 1;
         }
-        else
-        {
-            list = insert_node(number, list); // making a new head
-        }
+        n->value = number;
+        n->next = NULL;
+
+        list = (list == NULL) ? n : append_sorted(n, list); // if list is empty add node at beginning else add node to the list keeping sorted order
     }
 
     print_list(list);
     free_list(list);
 }
 
-node *make_list(int head)
+node *prepend_node(node *n, node *head)
 {
-    node *n = malloc(sizeof(node)); // dynamically allocating memory for current node
-
-    n->value = head;
-    n->next = NULL;
-    return n;
-}
-
-node *insert_node(int value, node *list)
-{
-    node *n = malloc(sizeof(node));
-
-    n->value = value;
-    n->next = list; // adding node beore the current head;
+    if (head != NULL) n->next = head; // adding node beore the current head;
 
     return n;
 }
 
-void print_list(node *list)
+node *append_node(node *n, node *head)
 {
-    node *ptr = list;
+    if (head == NULL)
+    {
+        return prepend_node(n, head);
+    }
+    node *ptr = head;
+    while(ptr != NULL)
+    {
+        if(ptr->next == NULL)
+        {
+            ptr->next = n;
+            break;
+        }
+        ptr = ptr->next;
+    }
+    return head;
+}
+
+node *append_sorted(node *n, node *head)
+{
+    if (head == NULL || head->value > n->value)
+    {
+        head = prepend_node(n, head); // if list is empty or curren node is smaller than first adding it to the beginning
+    }
+    else 
+    {
+        node *ptr = head;
+        while(ptr != NULL) // looping through the list
+        {
+            if(ptr->next == NULL) // if at end add node here
+            {
+                ptr->next = n;
+                break;
+            }
+            else if(ptr->next->value > n->value) // if next value is smaller than current add node here
+            {
+                n->next = ptr->next;
+                ptr->next = n;
+                break;
+            }
+            ptr = ptr->next; // incrementing the traversal ppointer
+        }
+    }
+    return head;
+}
+
+void print_list(node *head)
+{
+    node *ptr = head;
     while (ptr != NULL)
     {
         printf("%i ", ptr->value);
@@ -68,9 +109,9 @@ void print_list(node *list)
     printf("\n");
 }
 
-void free_list(node *list)
+void free_list(node *head)
 {
-    if (list == NULL) return;
-    free_list(list->next);
-    free(list);
+    if (head == NULL) return;
+    free_list(head->next);
+    free(head);
 }
